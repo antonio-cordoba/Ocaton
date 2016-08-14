a5e159cc1caa4c2338167a5cad9a5748085ed98e
acordoba <acordoba@yahoo.com>
2016-08-14 04:23:02 -0700

    - Initial Upload

---
+++ Equilibrium/ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] abc = { 1, 2, 3, 5, -2, 11 };
+            Console.WriteLine("Equilibrium value = {0}", FindEqu(abc));
+            Console.ReadKey();
+        }
+
+        static int FindEqu(int[] data)
+        {
+            int result = -1;
+
+            var calc = data
+                .Select((a, i) => new {
+                    pos = i,
+                    sumpre = data.Take(i).Sum(),
+                    sumsuf = data.Skip(i + 1).Sum()
+                })
+                .Where(c => c.sumpre == c.sumsuf)
+                .FirstOrDefault();
+
+            if (calc != null)
+                result = calc.pos;
+
+            return result;
+        }
+    }
+}
